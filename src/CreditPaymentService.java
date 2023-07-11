ab3ed81ecc59a128a60b11b54c09ccbe218267a3
Ekaterina Khritonenkova <Catybym@gmail.com>
2023-07-11 12:04:44 +0300

    commit edited

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,9 @@
 public class CreditPaymentService {
-    public double calculate(double k, double p, int m) {
+    public double calculate(double amountCredit, double interestRate, int loanTerm) {
         //  А=К*(П/(1+П)-М-1)
-        double sumСredit = k; // сумма кредита
-        double percent = p; // процентная ставка
-        int month = m; // количество месяцев
-        double everyMothPercent = p / 12 / 100; // ежемесячная проц.ставка на основе годовой
-        int quantity = m * 12; // количество платежей всего
-        double A = (k * everyMothPercent * Math.pow(1 + everyMothPercent, quantity)) / (Math.pow(1 + everyMothPercent, quantity) - 1); // формула расчета кредита
-        return A;
+        double everyMothPercent = interestRate / 12 / 100; // ежемесячная проц.ставка на основе годовой
+        int quantity = loanTerm * 12; // количество платежей всего
+        double monthlyPayment = (amountCredit * everyMothPercent * Math.pow(1 + everyMothPercent, quantity)) / (Math.pow(1 + everyMothPercent, quantity) - 1); // формула расчета кредита
+        return monthlyPayment;
     }
 }
